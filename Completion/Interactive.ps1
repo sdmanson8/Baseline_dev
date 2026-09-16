45c46abb32fb750f7ac1a1bbcb1b75629b607106
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2026-09-16 08:57:39 +0200

    Update version history in Interactive.ps1

--- Completion/Interactive.ps1
+++ Completion/Interactive.ps1
@@ -10,7 +10,8 @@
     21.03.2026 - Added GUI
     06.04.2026 - Major changes to the GUI, and added more features
     26.04.2026 - Minor Fixes
-    unreleased - unreleased
+    24.05.2026 - Major changes to the GUI, and added more features
+	16.09.2026 - Minor Fixes
 
     .AUTHOR
     sdmanson8 - Copyright (c) 2026
