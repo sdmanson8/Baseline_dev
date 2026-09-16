7463542fc845413e0e3879f0c6d6b2f58b9294f7
sdmanson8 <90516190+sdmanson8@users.noreply.github.com>
2026-09-16 08:57:16 +0200

    Add entry for minor fixes on 16.09.2026

--- Bootstrap/Baseline.ps1
+++ Bootstrap/Baseline.ps1
@@ -11,6 +11,7 @@
     06.04.2026 - Major changes to the GUI, and added more features
     26.04.2026 - Minor Fixes
     24.05.2026 - Major changes to the GUI, and added more features
+	16.09.2026 - Minor Fixes
 
 	.AUTHOR
 	sdmanson8 - Copyright (c) 2026
